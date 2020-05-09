9a9d7bda536d826d736747675196c9f7ccb7626e
Imtiaz <imtiazali4090@gmail.com>
2020-05-09 19:53:49 +0600

    No Comment

--- views/admin_customer_management.php
+++ views/admin_customer_management.php
@@ -19,7 +19,7 @@
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
+          <li class="nav-item">
             <a class="nav-link" href="admin_dashboard.php">Dashboard<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
@@ -28,7 +28,7 @@
           <li class="nav-item">
             <a class="nav-link" href="admin_employee_management.php">Employees Management</a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item active">
             <a class="nav-link" href="admin_customer_management.php">Customer Management</a>
           </li>
         </ul>
