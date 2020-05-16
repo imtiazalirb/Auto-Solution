d324228b609502358497c51907afce70c415ee4f
Imtiaz <imtiazali4090@gmail.com>
2020-05-16 23:57:35 +0600

    Fixed linkup of employee list

--- views/admin_dashboard.php
+++ views/admin_dashboard.php
@@ -51,7 +51,7 @@ else
               Employee Management
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">All Employee</a>
+              <a class="dropdown-item" href="admin_all_employee.php">All Employee</a>
               <a class="dropdown-item" href="employee_signup.php">Assign Employee</a>
               <a class="dropdown-item" href="#">Edit Employee Detail</a>
             </div>
