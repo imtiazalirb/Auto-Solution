26fc5745af54508aefcebbe5bd85ab24f42b6601
Imtiaz <imtiazali4090@gmail.com>
2020-05-28 14:55:01 +0600

    Updated DB structure

--- auto_solution.sql
+++ auto_solution.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 24, 2020 at 07:04 PM
+-- Generation Time: May 28, 2020 at 10:54 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
@@ -46,7 +46,7 @@ CREATE TABLE `car` (
   `image` varchar(500) DEFAULT '../storage/no_image.jpg',
   `last_serviced_item` varchar(100) DEFAULT NULL,
   `last_serviced_date` date DEFAULT NULL,
-  `last_serviced_odo` int(10) DEFAULT NULL
+  `last_serviced_odo` varchar(12) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -54,13 +54,14 @@ CREATE TABLE `car` (
 --
 
 INSERT INTO `car` (`car_id`, `user_id`, `username`, `car_make`, `car_model`, `trim`, `body_type`, `year`, `color`, `drivetrain`, `engine_type`, `fuel_type`, `engine_no`, `registration_no`, `vin_no`, `image`, `last_serviced_item`, `last_serviced_date`, `last_serviced_odo`) VALUES
-(27, 2, 'imtiaz', 'Toyota', 'Chaser', 'Tourer V', 'Sedan', 1998, 'White', 'RWD', 'Inline 6', 'Gasoline', 'JZ43093004034', 'DHK GA-16-4111', '2344253585453', '../storage/car_image/bYAAAgClyOA-1920.jpg', '', NULL, NULL),
-(28, 2, 'imtiaz', 'Toyota', 'Supra', 'MK4 Twin Turbo', 'Coupe', 1998, 'White', 'RWD', 'Inline 6', 'Gasoline', 'JZ2344564356523', 'DHK VHA-13-4522', 'Q3R4W423432556', '../storage/car_image/430691-toyota-supra-wallpaper-2048x1365-1080p.jpg', '', NULL, NULL),
-(29, 6, 'shafat', 'Volkswagen', 'Golf', 'R', 'Hatchback', 2020, 'Blue', 'AWD', 'Inline 4', 'Gasoline', '2498593495', 'CTG GA-32-4433', '3259839485934', '../storage/car_image/Volkswagen-Golf-R-5door-2017-1920x1200-001.jpg', '', NULL, NULL),
-(30, 6, 'shafat', 'Toyota', 'Corolla', 'X Assista', 'Sedan', 2006, 'White', 'FWD', 'Inline 4', 'Gasoline', '293102940190', 'DHK GA-23-4554', '', '../storage/car_image/37c1b6ds-960.jpg', '', NULL, NULL),
-(32, 2, 'imtiaz', 'Ford', 'F150', 'Raptor', 'Truck', 2019, 'Red', '4WD', 'V6', 'Gasoline', '234324234', 'DHK THA-11-9982', '', '../storage/car_image/i.webp', '', NULL, NULL),
-(33, 2, 'imtiaz', 'Mitsubishi', 'Lancer Evo 10', 'FQ400', 'Sedan', 2010, 'Blue', 'AWD', 'Inline 4', 'Gasoline', '13252345', 'DHK GA-13-4444', '', '../storage/car_image/Mitsubishi-Lancer-EVO-X-FQ-400-16.jpg', '', NULL, NULL),
-(34, 2, 'imtiaz', 'BMW', 'M3', 'E30', 'Sedan', 1987, 'Black', 'RWD', 'Inline 4', 'Flex', '32402390', 'DHK GA-13-4567', '', '../storage/car_image/E30-M3-Photo-gallery26.jpg', '', NULL, NULL);
+(27, 1, 'imtiaz', 'Toyota', 'Chaser', 'Tourer V', 'Sedan', 1998, 'White', 'RWD', 'Inline 6', 'Gasoline', 'JZ43093004034', 'DHK GA-16-4111', '2344253585453', '../storage/car_image/bYAAAgClyOA-1920.jpg', NULL, NULL, NULL),
+(28, 1, 'imtiaz', 'Toyota', 'Supra', 'MK4 Twin Turbo', 'Coupe', 1998, 'White', 'RWD', 'Inline 6', 'Gasoline', 'JZ2344564356523', 'DHK VHA-13-4522', 'Q3R4W423432556', '../storage/car_image/430691-toyota-supra-wallpaper-2048x1365-1080p.jpg', NULL, NULL, NULL),
+(29, 2, 'shafat', 'Volkswagen', 'Golf', 'R', 'Hatchback', 2020, 'Blue', 'AWD', 'Inline 4', 'Gasoline', '2498593495', 'CTG GA-32-4433', '3259839485934', '../storage/car_image/Volkswagen-Golf-R-5door-2017-1920x1200-001.jpg', NULL, NULL, NULL),
+(30, 2, 'shafat', 'Toyota', 'Corolla', 'X Assista', 'Sedan', 2006, 'White', 'FWD', 'Inline 4', 'Gasoline', '293102940190', 'DHK GA-23-4554', '', '../storage/car_image/37c1b6ds-960.jpg', NULL, NULL, NULL),
+(32, 1, 'imtiaz', 'Ford', 'F150', 'Raptor', 'Truck', 2019, 'Red', '4WD', 'V6', 'Gasoline', '234324234', 'DHK THA-11-9982', '', '../storage/car_image/i.webp', NULL, NULL, NULL),
+(33, 1, 'imtiaz', 'Mitsubishi', 'Lancer Evo 10', 'FQ400', 'Sedan', 2010, 'Blue', 'AWD', 'Inline 4', 'Gasoline', '13252345', 'DHK GA-13-4444', '', '../storage/car_image/Mitsubishi-Lancer-EVO-X-FQ-400-16.jpg', NULL, NULL, NULL),
+(34, 1, 'imtiaz', 'BMW', 'M3', 'E30', 'Sedan', 1987, 'Black', 'RWD', 'Inline 4', 'Flex', '32402390', 'DHK GA-13-4567', '', '../storage/car_image/E30-M3-Photo-gallery26.jpg', NULL, NULL, NULL),
+(41, 1, 'imtiaz', 'AUDI', 'R8', 'V10 Plus', 'Coupe', 2019, 'Red', 'AWD', 'V10', 'Gasoline', '324324234', 'SW R8V10', '123343567', '../storage/car_image/2017-Audi-R8-JY-01.jpg', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -91,9 +92,8 @@ CREATE TABLE `employees_detail` (
 --
 
 INSERT INTO `employees_detail` (`id`, `first_name`, `last_name`, `birth_date`, `gender`, `email`, `phone`, `designation`, `salary`, `street`, `city`, `state`, `zip`, `username`, `password`) VALUES
-(7, 'Mahmud', 'Zaman', '1996-02-06', 'Male', 'mahmud@yahoo.com', '+8801716402222', 'Other', 10000, '12/1 Moddho Badda', 'Dhaka', 'Dhaka', '1226', 'mahmud', '$2y$10$8uEyPPH1fVtqfJYEFJDU8u3lirqVRlEFJcL4N2L5Dpc1QRY/8HTEO'),
-(4, 'Abdus', 'Samad', '1984-02-29', 'Male', 'samad@gmail.com', '+8801915647629', 'Manager', 40000, '129/A/12 Basundhara Abasik', 'Dhaka', 'Dhaka', '1216', 'samad', '$2y$10$FEdFtKayoOSgPZfJT4rGFuWGzxP8JMYMTQc.2HZSay7mONknZ4lE6'),
-(6, 'Zulhas', 'Molla', '1990-03-15', 'Male', 'zulhas@gmail.com', '+880171982875800', 'Mechanic', 22000, '129/A/12 Basundhara Abasik', 'Dhaka', 'Dhaka', '1229', 'zulhas', '$2y$10$apT0DGKxkIQF2iMj8DJEcum38s7.7oP4KPIswOznsW7sGn/O7yYNS');
+(9, 'Abdus', 'Samad', '1984-02-29', 'Male', 'samad@gmail.com', '+8801915647629', 'Manager', 30000, '129/A/12 Basundhara Abasik', 'Dhaka', 'Dhaka', '1216', 'samad', '$2y$10$FEdFtKayoOSgPZfJT4rGFuWGzxP8JMYMTQc.2HZSay7mONknZ4lE6'),
+(10, 'Zulhas', 'Molla', '1999-03-03', 'Male', 'zulhas@gmail.com', '+8801915647624', 'Service Engineer', 20316, '120 Janata Housing, Mirpur-1', 'Dhaka', 'Dhaka', '1216', 'zulhas', '$2y$10$t9.iTtuOT9fg2/iLuFrii.GefaA28f7LBetgS8jeV/wu5iQomDjvO');
 
 -- --------------------------------------------------------
 
@@ -113,11 +113,11 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`id`, `username`, `password`, `type`) VALUES
-(2, 'imtiaz', '$2y$10$ZLPWvX81rEOZQSo26Iv6Keruinmok5eWh7nZKXdbduMuYinPVegbK', 'user'),
-(5, 'zulhas', '$2y$10$eXCswhWCyUtkCpizlW38fucS2ZPnrmirhzTqXHa4rg/VSTnNXBgie', 'employee'),
-(6, 'shafat', '$2y$10$aRWly25zdpo4uD.NDzy77uSIN9y8vgDOsEiHikiKEETVTK.JDai6a', 'user'),
-(8, 'samad', '$2y$10$FEdFtKayoOSgPZfJT4rGFuWGzxP8JMYMTQc.2HZSay7mONknZ4lE6', 'employee'),
-(11, 'mahmud', '$2y$10$8uEyPPH1fVtqfJYEFJDU8u3lirqVRlEFJcL4N2L5Dpc1QRY/8HTEO', 'employee');
+(1, 'imtiaz', '$2y$10$ZLPWvX81rEOZQSo26Iv6Keruinmok5eWh7nZKXdbduMuYinPVegbK', 'user'),
+(2, 'shafat', '$2y$10$aRWly25zdpo4uD.NDzy77uSIN9y8vgDOsEiHikiKEETVTK.JDai6a', 'user'),
+(9, 'samad', '$2y$10$FEdFtKayoOSgPZfJT4rGFuWGzxP8JMYMTQc.2HZSay7mONknZ4lE6', 'employee'),
+(13, 'zulhas', '$2y$10$t9.iTtuOT9fg2/iLuFrii.GefaA28f7LBetgS8jeV/wu5iQomDjvO', 'employee'),
+(14, 'nur', '$2y$10$1eiy4i/oDayjHemg14JLIOfWzzV9..zI/urrxMJQsVsK2.e01Pk/e', 'user');
 
 -- --------------------------------------------------------
 
@@ -162,12 +162,12 @@ CREATE TABLE `service_request` (
   `car_id` int(12) NOT NULL,
   `user_id` int(12) NOT NULL,
   `name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
   `street` varchar(100) NOT NULL,
   `city` varchar(50) NOT NULL,
   `zip` int(12) NOT NULL,
   `phone` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `username` varchar(50) NOT NULL,
   `car_detail` varchar(50) NOT NULL,
   `body_type` varchar(50) NOT NULL,
   `year` int(4) NOT NULL,
@@ -183,18 +183,29 @@ CREATE TABLE `service_request` (
   `last_serviced_item` varchar(50) DEFAULT NULL,
   `request_service_date` date NOT NULL DEFAULT current_timestamp(),
   `last_serviced_date` date DEFAULT NULL,
-  `last_serviced_odo` int(10) DEFAULT NULL
+  `last_serviced_odo` int(10) DEFAULT NULL,
+  `mechanic_message` varchar(100) DEFAULT NULL,
+  `serviced_by` varchar(50) DEFAULT NULL,
+  `employee_username` varchar(50) DEFAULT NULL,
+  `servicing_stage` int(4) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `service_request`
 --
 
-INSERT INTO `service_request` (`service_request_id`, `car_id`, `user_id`, `name`, `street`, `city`, `zip`, `phone`, `email`, `username`, `car_detail`, `body_type`, `year`, `color`, `drivetrain`, `engine_type`, `fuel_type`, `engine_no`, `registration_no`, `vin_no`, `service_needed`, `message`, `last_serviced_item`, `request_service_date`, `last_serviced_date`, `last_serviced_odo`) VALUES
-(10, 34, 2, '', '', '', 0, '', '', 'imtiaz', 'BMW M3 E30', 'Sedan', 1987, 'Black', 'RWD', 'Inline 4', 'Flex', '32402390', 'DHK GA-13-4567', '', '[Brakes] ', '', '', '2020-05-24', '0000-00-00', 0),
-(12, 32, 2, '', '', '', 0, '', '', 'imtiaz', 'Ford F150 Raptor', 'Truck', 2019, 'Red', '4WD', 'V6', 'Gasoline', '234324234', 'DHK THA-11-9982', '', '[Suspension] ', 'Need Fox OEM shocks', '', '2020-05-24', '0000-00-00', 0),
-(13, 33, 2, '', '', '', 0, '', '', 'imtiaz', 'Mitsubishi Lancer Evo 10 FQ400', 'Sedan', 2010, 'Blue', 'AWD', 'Inline 4', 'Gasoline', '13252345', 'DHK GA-13-4444', '', '[Battery] [Clutch] [Cambelt] ', 'Please use OEM parts only', '', '2020-05-24', '0000-00-00', 0),
-(16, 27, 2, 'Imtiaz Ali', 'imtiaz', 'H-111, Road-4, Sangbadik Abasik Elaka', 0, '1216', '+8801625036306', 'imtiazali4090@gmail.com', 'Toyota Chaser Tourer V', 'Sedan', 1998, 'White', 'RWD', 'Inline 6', 'Gasoline', 'JZ43093004034', 'DHK GA-16-4111', '2344253585453', '[Minor service] ', '', '', '2020-05-24', '0000-00-00', 0);
+INSERT INTO `service_request` (`service_request_id`, `car_id`, `user_id`, `name`, `username`, `street`, `city`, `zip`, `phone`, `email`, `car_detail`, `body_type`, `year`, `color`, `drivetrain`, `engine_type`, `fuel_type`, `engine_no`, `registration_no`, `vin_no`, `service_needed`, `message`, `last_serviced_item`, `request_service_date`, `last_serviced_date`, `last_serviced_odo`, `mechanic_message`, `serviced_by`, `employee_username`, `servicing_stage`) VALUES
+(19, 28, 1, 'Imtiaz Ali', 'imtiaz', 'H-111, Road-4, Sangbadik Abasik Elaka', 'Dhaka', 1216, '+8801625036306', 'imtiazali4090@gmail.com', 'Toyota Supra MK4 Twin Turbo', 'Coupe', 1998, 'White', 'RWD', 'Inline 6', 'Gasoline', 'JZ2344564356523', 'DHK VHA-13-4522', 'Q3R4W423432556', '[Paint] ', '', '[Paint] ', '2020-05-26', '2020-05-28', 8000, 'nothing', 'Abdus Samad', NULL, 2),
+(21, 28, 1, 'Imtiaz Ali', 'imtiaz', 'H-111, Road-4, Sangbadik Abasik Elaka', 'Dhaka', 1216, '+8801625036306', 'imtiazali4090@gmail.com', 'Toyota Supra MK4 Twin Turbo', 'Coupe', 1998, 'White', 'RWD', 'Inline 6', 'Gasoline', 'JZ2344564356523', 'DHK VHA-13-4522', 'Q3R4W423432556', '[Paint] [Battery] [Other] ', '', '[Paint] [Battery] [Other] ', '2020-05-27', '2020-05-28', 60000, '', 'Zulhas Molla', NULL, 2),
+(22, 32, 1, 'Imtiaz Ali', 'imtiaz', 'H-111, Road-4, Sangbadik Abasik Elaka', 'Dhaka', 1216, '+8801625036306', 'imtiazali4090@gmail.com', 'Ford F150 Raptor', 'Truck', 2019, 'Red', '4WD', 'V6', 'Gasoline', '234324234', 'DHK THA-11-9982', '', '[Suspension] ', 'Need OEM Fox suspension setup', '', '2020-05-27', '0000-00-00', 0, NULL, NULL, NULL, 0),
+(25, 33, 1, 'Imtiaz Ali', 'imtiaz', 'H-111, Road-4, Sangbadik Abasik Elaka', 'Dhaka', 1216, '+8801625036306', 'imtiazali4090@gmail.com', 'Mitsubishi Lancer Evo 10 FQ400', 'Sedan', 2010, 'Blue', 'AWD', 'Inline 4', 'Gasoline', '13252345', 'DHK GA-13-4444', '', '', '', '', '2020-05-27', '0000-00-00', 0, NULL, NULL, NULL, 0),
+(26, 33, 1, 'Imtiaz Ali', 'imtiaz', 'H-111, Road-4, Sangbadik Abasik Elaka', 'Dhaka', 1216, '+8801625036306', 'imtiazali4090@gmail.com', 'Mitsubishi Lancer Evo 10 FQ400', 'Sedan', 2010, 'Blue', 'AWD', 'Inline 4', 'Gasoline', '13252345', 'DHK GA-13-4444', '', '[Body dent] ', '', '', '2020-05-27', '0000-00-00', 0, NULL, NULL, NULL, 0),
+(27, 34, 1, 'Imtiaz Ali', 'imtiaz', 'H-111, Road-4, Sangbadik Abasik Elaka', 'Dhaka', 1216, '+8801625036306', 'imtiazali4090@gmail.com', 'BMW M3 E30', 'Sedan', 1987, 'Black', 'RWD', 'Inline 4', 'Flex', '32402390', 'DHK GA-13-4567', '', '[Cambelt] ', '', '', '2020-05-28', '0000-00-00', 0, NULL, NULL, NULL, 0),
+(28, 30, 2, 'Shafayat Ul Islam', 'shafat', '135/5 Sangbadik R/A, Mirpur-11', 'Dhaka', 1216, '+88017198287580', 'shafat46r@gmail.com', 'Toyota Corolla X Assista', 'Sedan', 2006, 'White', 'FWD', 'Inline 4', 'Gasoline', '293102940190', 'DHK GA-23-4554', '', '[Body dent] ', '', '[Body dent] ', '2020-05-28', '2020-05-28', 45464, '', 'Zulhas Molla', 'zulhas', 2),
+(29, 30, 2, 'Shafayat Ul Islam', 'shafat', '135/5 Sangbadik R/A, Mirpur-11', 'Dhaka', 1216, '+88017198287580', 'shafat46r@gmail.com', 'Toyota Corolla X Assista', 'Sedan', 2006, 'White', 'FWD', 'Inline 4', 'Gasoline', '293102940190', 'DHK GA-23-4554', '', '[Engine] ', 'Check engine light is on', '', '2020-05-28', '0000-00-00', 0, '', '', NULL, 0),
+(30, 29, 2, 'Shafayat Ul Islam', 'shafat', '135/5 Sangbadik R/A, Mirpur-11', 'Dhaka', 1216, '+88017198287580', 'shafat46r@gmail.com', 'Volkswagen Golf R', 'Hatchback', 2020, 'Blue', 'AWD', 'Inline 4', 'Gasoline', '2498593495', 'CTG GA-32-4433', '3259839485934', '[Paint] ', '', '', '2020-05-28', '0000-00-00', 0, '', '', NULL, 0),
+(31, 29, 2, 'Shafayat Ul Islam', 'shafat', '135/5 Sangbadik R/A, Mirpur-11', 'Dhaka', 1216, '+88017198287580', 'shafat46r@gmail.com', 'Volkswagen Golf R', 'Hatchback', 2020, 'Blue', 'AWD', 'Inline 4', 'Gasoline', '2498593495', 'CTG GA-32-4433', '3259839485934', '[Engine] ', 'Check engine light is on', '[Engine] ', '2020-05-28', '2020-05-28', 50000, 'Fixed MAP sensor which caused engine light', 'Zulhas Molla', NULL, 2),
+(32, 32, 1, 'Imtiaz Ali', 'imtiaz', 'H-111, Road-4, Sangbadik Abasik Elaka', 'Dhaka', 1216, '+8801625036306', 'imtiazali4090@gmail.com', 'Ford F150 Raptor', 'Truck', 2019, 'Red', '4WD', 'V6', 'Gasoline', '234324234', 'DHK THA-11-9982', '', '[Suspension] ', '', '[Suspension] ', '2020-05-28', '2020-05-28', 9000, '', 'Abdus Samad', 'samad', 2);
 
 -- --------------------------------------------------------
 
@@ -223,8 +234,9 @@ CREATE TABLE `users_detail` (
 --
 
 INSERT INTO `users_detail` (`id`, `first_name`, `last_name`, `birth_date`, `gender`, `email`, `phone`, `street`, `city`, `state`, `zip`, `username`, `password`) VALUES
-(2, 'Imtiaz', 'Ali', '1996-07-09', 'Male', 'imtiazali4090@gmail.com', '+8801625036306', 'H-111, Road-4, Sangbadik Abasik Elaka', 'Dhaka', 'Dhaka', '1216', 'imtiaz', '$2y$10$ZLPWvX81rEOZQSo26Iv6Keruinmok5eWh7nZKXdbduMuYinPVegbK'),
-(4, 'Shafayat Ul', 'Islam', '1996-11-01', 'Male', 'shafat46r@gmail.com', '+88017198287580', '135/5 Sangbadik R/A, Mirpur-11', 'Dhaka', 'Dhaka', '1216', 'shafat', '$2y$10$aRWly25zdpo4uD.NDzy77uSIN9y8vgDOsEiHikiKEETVTK.JDai6a');
+(1, 'Imtiaz', 'Ali', '1996-07-09', 'Male', 'imtiazali4090@gmail.com', '+8801625036306', 'H-111, Road-4, Sangbadik Abasik Elaka', 'Dhaka', 'Dhaka', '1216', 'imtiaz', '$2y$10$ZLPWvX81rEOZQSo26Iv6Keruinmok5eWh7nZKXdbduMuYinPVegbK'),
+(5, 'Nur', 'Mohammad', '2020-05-20', 'Male', 'nur@gmail.com', '+88019156476243', '120 Janata Housing, Mirpur-1', 'Dhaka', 'Dhaka', '1216', 'nur', '$2y$10$1eiy4i/oDayjHemg14JLIOfWzzV9..zI/urrxMJQsVsK2.e01Pk/e'),
+(2, 'Shafayat Ul', 'Islam', '1996-11-01', 'Male', 'shafat46r@gmail.com', '+88017198287580', '135/5 Sangbadik R/A, Mirpur-11', 'Dhaka', 'Dhaka', '1216', 'shafat', '$2y$10$aRWly25zdpo4uD.NDzy77uSIN9y8vgDOsEiHikiKEETVTK.JDai6a');
 
 --
 -- Indexes for dumped tables
@@ -282,19 +294,19 @@ ALTER TABLE `users_detail`
 -- AUTO_INCREMENT for table `car`
 --
 ALTER TABLE `car`
-  MODIFY `car_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `car_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `employees_detail`
 --
 ALTER TABLE `employees_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `quote`
@@ -306,13 +318,13 @@ ALTER TABLE `quote`
 -- AUTO_INCREMENT for table `service_request`
 --
 ALTER TABLE `service_request`
-  MODIFY `service_request_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `service_request_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `users_detail`
 --
 ALTER TABLE `users_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
